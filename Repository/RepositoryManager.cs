cbbe7e4dd2db0f8c91ce83e29becf00f739bc467
Aiden Miller <bonsoirelliot@gmail.com>
2022-02-17 11:49:38 -0500

    repository manager added

---
+++ Repository/RepositoryManager.cs
@@ -0,0 +1,41 @@
+﻿using Contracts;
+using Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository
+{
+    public class RepositoryManager : IRepositoryManager
+    {
+        private RepositoryContext _repositoryContext;
+        private IUserRepository? _companyRepository;
+        private ITaskRepository? _employeeRepository;
+        public RepositoryManager(RepositoryContext repositoryContext)
+        {
+            _repositoryContext = repositoryContext;
+        }
+        public IUserRepository User
+        {
+            get
+            {
+                if (_companyRepository == null)
+                    _companyRepository = new UserRepository(_repositoryContext);
+                return _companyRepository;
+            }
+        }
+        public ITaskRepository Task
+        {
+            get
+            {
+                if (_employeeRepository == null)
+                    _employeeRepository = new TaskRepository(_repositoryContext);
+                return _employeeRepository;
+            }
+        }
+        public void Save() => _repositoryContext.SaveChanges();
+    }
+
+}
